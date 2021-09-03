6b870e488717a86a1ebb6d30ddf94f78acadf90f
Jane Fraser <janef0421@icloud.com>
2021-09-03 14:14:49 +1200

    Hello Universe is now loaded from a bundle

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HelloUniverse",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+		.copy("message.txt")
+	]),
         .testTarget(
             name: "HelloUniverseTests",
             dependencies: ["HelloUniverse"]),
--- Sources/HelloUniverse/main.swift
+++ Sources/HelloUniverse/main.swift
@@ -1,1 +1,5 @@
-print("Hello Universe")
+import Foundation
+
+if let url = Bundle.module.url(forResource: "message", withExtension: "txt"), let data = try? Data.init(contentsOf: url), let message = String(data: data, encoding: .utf8) {
+	print(message)
+}
